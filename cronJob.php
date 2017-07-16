5f827358f7aab1b4e5b3252dec9910dbafea14c1
Peter Clayder <peterclayder@gmail.com>
2017-07-16 20:40:39 -0300

    Inseri um intervalo para o envio de mensagens

--- cronJob.php
+++ cronJob.php
@@ -1,5 +1,18 @@
 <?php
 require 'autoload.php';
-$dicionario = new \app\controller\Dicionario();
 
-$dicionario->getPalavra();
+$hora = new DateTime("now");
+$min = new DateTime("8:00:00");
+$max = new DateTime("23:59:00");
+
+$horaAtual = $hora->format("H:i:s");
+$horaMin = $min->format("H:i:s");
+$horaMax = $max->format("H:i:s");
+
+// só envia a mensagem entre a hora minima e maxima
+if($horaAtual > $horaMin  && $horaAtual < $horaMax){
+    $dicionario = new \app\controller\Dicionario();
+    $dicionario->getPalavra();
+}
+
+
